0a1d23d5b6a5150c6e11da765bcdb2de4f4c2420
Чигиринцев Борис <crm.boris@gmail.com>
2025-08-07 18:49:16 +1000

    1

--- avterminal-api/app/Http/Controllers/AmoCRMController.php
+++ avterminal-api/app/Http/Controllers/AmoCRMController.php
@@ -182,4 +182,94 @@ class AmoCRMController extends Controller
 
         return $formattedFields;
     }
+
+    public function generateXmlByLeadId(Request $request, $id)
+    {
+        try {
+            $leadData = $this->getFormattedLeadAndContactData($request, $id)->getData(true);
+
+            $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="windows-1251"?><AltaPassengerDeclaration/>');
+
+            $this->addXmlData($xml, $leadData, $id);
+
+            $headers = [
+                'Content-Type' => 'application/xml; charset=windows-1251',
+                'Content-Disposition' => 'attachment; filename="lead_' . $id . '.xml"',
+            ];
+
+            return response($xml->asXML(), 200, $headers);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    private function addXmlData(\SimpleXMLElement $xml, array $data, $leadId)
+    {
+        $fields = [];
+        foreach ($data['custom_fields'] as $field) {
+            $fields[$field['field_name']] = $field['field_value'];
+        }
+
+        // DeclarantPerson
+        $xml->addChild('DeclarantPerson_PersonSurname', $fields['Фамилия'] ?? '');
+        $xml->addChild('DeclarantPerson_PersonName', $fields['Имя'] ?? '');
+        $xml->addChild('DeclarantPerson_PersonMiddleName', $fields['Отчество'] ?? '');
+        $xml->addChild('DeclarantPerson_BirthDate', $fields['День рождения'] ?? '');
+        $xml->addChild('DeclarantPerson_INN', $fields['ИНН'] ?? '');
+
+        // DeclarantPerson_PersonIdCard
+        $xml->addChild('DeclarantPerson_PersonIdCard_IdentityCardCode', 'RU01001');
+        $xml->addChild('DeclarantPerson_PersonIdCard_IdentityCardName', 'ПАСПОРТ');
+        $xml->addChild('DeclarantPerson_PersonIdCard_FullIdentityCardName', 'ПАСПОРТ ГРАЖДАНИНА РФ');
+        $xml->addChild('DeclarantPerson_PersonIdCard_CountryCode', 'RU');
+        $xml->addChild('DeclarantPerson_PersonIdCard_IdentityCardSeries', $fields['Серия паспорта'] ?? '');
+        $xml->addChild('DeclarantPerson_PersonIdCard_IdentityCardNumber', $fields['Номер паспорта'] ?? '');
+        $xml->addChild('DeclarantPerson_PersonIdCard_OrganizationName', $fields['Кем выдан'] ?? '');
+        $xml->addChild('DeclarantPerson_PersonIdCard_IdentityCardDate', $fields['Дата выдачи'] ?? '');
+        $xml->addChild('DeclarantPerson_PersonIdCard_IssuerCode', $fields['Код подразделения'] ?? '');
+
+        // DeclarantPerson_Address
+        $address = $xml->addChild('DeclarantPerson_Address');
+        $address->addChild('AddressKindCode', '1');
+        $address->addChild('Region', $fields['Субъект федерации'] ?? '');
+        $address->addChild('District', $fields['Район'] ?? '');
+        $address->addChild('Town', $fields['Город'] ?? '');
+        $address->addChild('City', '');
+        $address->addChild('StreetHouse', $fields['Улица'] ?? '');
+        $address->addChild('House', $fields['Дом'] ?? '');
+        $address->addChild('Room', $fields['Квартира'] ?? '');
+        $address->addChild('CountryCode', 'RU');
+        $address->addChild('CounryName', 'РОССИЯ');
+
+        // TransportMeans
+        $xml->addChild('TransportMeans_TransferPurposeCode', '1');
+        $transportDetails = $xml->addChild('TransportMeans_TransportMeansDetails');
+        $transportDetails->addChild('Mark', $fields['Марка'] ?? '');
+        $transportDetails->addChild('Model', $fields['Модель'] ?? '');
+        $transportDetails->addChild('VINID', $fields['VIN'] ?? '');
+        $transportDetails->addChild('BodyID', $fields['VIN'] ?? '');
+        $transportDetails->addChild('TransportModeCode', '30');
+        $transportDetails->addChild('TransportMeansRegId', 'ОТСУТСТВУЕТ');
+        $transportDetails->addChild('ChassisID', 'ОТСУТСТВУЕТ');
+        $transportDetails->addChild('TypeIndicator', '1');
+
+        // FilledPerson_SigningDetails
+        $xml->addChild('FilledPerson_SigningDetails_PersonSurname', 'ПОЛУЭКТОВ');
+        $xml->addChild('FilledPerson_SigningDetails_PersonName', 'ВИТАЛИЙ');
+        $xml->addChild('FilledPerson_SigningDetails_PersonMiddleName', 'СЕРГЕЕВИЧ');
+        $xml->addChild('FilledPerson_SigningDetails_PersonPost', 'ГЕНЕРАЛЬНЫЙ ДИРЕКТОР');
+
+        // RoleCode
+        $xml->addChild('RoleCode', '2');
+
+        // SignatoryRepresentativeDetails
+        $xml->addChild('SignatoryRepresentativeDetails_BrokerRegistryDocDetails_DocKindCode', '09034');
+        $xml->addChild('SignatoryRepresentativeDetails_BrokerRegistryDocDetails_RegistrationNumberId', '1695');
+        $xml->addChild('SignatoryRepresentativeDetails_RepresentativeContractDetails_PrDocumentName', 'ДОГОВОР С ТАМОЖЕННЫМ ПРЕДСТАВИТЕЛЕМ');
+        $xml->addChild('SignatoryRepresentativeDetails_RepresentativeContractDetails_PrDocumentNumber', $leadId);
+        $xml->addChild('SignatoryRepresentativeDetails_RepresentativeContractDetails_PrDocumentDate', date('Y-m-d'));
+
+        // ElectronicDocumentSign
+        $xml->addChild('ElectronicDocumentSign', 'ЭД');
+    }
 }
